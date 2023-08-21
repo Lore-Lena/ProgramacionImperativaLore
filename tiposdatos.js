e064245bfb1de1fb4c6381795f028bb4bbb3262a
Lore-Lena <lerb90@gmail.com>
2023-08-20 21:50:23 -0600

    actualizando aprendizaje

---
+++ tiposdatos.js
@@ -0,0 +1,55 @@
+// numero
+let edad = 32
+let precio = 150.65
+let edad1 = 33
+console.log(edad1)
+console.log(precio)
+
+// strings cualquier longitud con comillas simples y dobles
+let nombre = "Lena"
+let apellido = 'Rodriguez'
+console.log(nombre)
+console.log(apellido)
+
+// booleanos - verdadero o falso
+let llueve = false
+let despejado = true
+console.log(llueve)
+console.log(despejado)
+
+// especiales
+// not a number - no es un texto, pero tampoco un booleano NAN
+let notANumber = NaN
+console.log(notANumber)
+
+// null define que dicho dato es dezconocido
+let nulo = null
+console.log(null)
+
+// Undefined - no se asocia a una variable, viene implicito si a la variable no se le asigno un valor
+let numeroCasa
+console.log(numeroCasa)
+numeroCasa=20
+console.log(numeroCasa)
+
+// objetos literales
+let miComputadora = {
+    marca: 'Mackbook Pro',
+    modelo: 2017,
+    estaEncendida: true
+}
+console.log(miComputadora)
+
+let persona = {
+    nombre: 'Lorena',
+    edad: 30
+}
+console.log(persona)
+
+// array, listo o arreglo - permite hacer algo similar al objeto literal. No tiene claves, sino indices numericos que inician en 0
+
+let paises = ["México", "Argentina", "Cuba", "China", "Colombia"]
+console.log(paises)
+let seriesFavoritas = ["Breaking Bad", "Friends"]
+console.log(seriesFavoritas)
+
---
+++ variables.js
@@ -0,0 +1,16 @@
+var nombre = "Lore"
+
+console.log(nombre)
+
+let numeroPreferido = 7
+numeroPreferido = 6
+console.log(numeroPreferido)
+
+if (true){
+    let numeroPreferido = 5 
+    console.log(numeroPreferido)
+}
+
+const apellido = "Rodriguez"
+console.log(apellido)
+
